273e54eb5744df9d0992436397df3fd25c1dfbd9
Victor Yap <victoryapcheeann@gmail.com>
2018-08-01 14:40:26 +0800

    second commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/* Step 1) Setting up
+    1) Download mongodb
+    2) Put the downloaded folder into your home folder
+    3) Renew it to mongo
+    4) Create a folder named mongo-data
+    5) Go to terminal - cd mongo/bin
+    6) ./mongod --dbpath ~/mongo-data
+    7) Open a new tab - Connect to the server u have just run
+    8) ./mongo - Just like node, you can ran the command 
+    9) db.test.insert({name:"John"})
+    10) db.test.find() - Mongodb give u a free default id
+    11) Install Robomongo
+    12) Use it to create collection, much easier than the command prompt way
+    13) npm init - Create json file
+    14) npm install --save mongodb@2.2.31
+*/
+
